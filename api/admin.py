4f6b8991b6395e2368c33d7756dc43e57bed6069
IbragimKhaniev <newinterny@gmail.com>
2025-11-11 13:20:35 +0300

    admin: register Ad and Favorite with configured ModelAdmin (list_display, filters, search_fields)

--- api/admin.py
+++ api/admin.py
@@ -1,3 +1,16 @@
 from django.contrib import admin
 
-# Register your models here.
+from .models import Ad, Favorite
+
+
+@admin.register(Ad)
+class AdAdmin(admin.ModelAdmin):
+    list_display = ("id", "title", "owner", "price", "is_approved", "created_at")
+    list_filter = ("is_approved",)
+    search_fields = ("title", "description", "owner__username")
+
+
+@admin.register(Favorite)
+class FavoriteAdmin(admin.ModelAdmin):
+    list_display = ("id", "user", "ad", "created_at")
+    search_fields = ("user__username", "ad__title")
